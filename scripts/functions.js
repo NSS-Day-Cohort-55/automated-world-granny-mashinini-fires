dca32f5a685697652c89229a42e7b1c4aaf23fff
ActuallyAJL <zero3260@hotmail.com>
2022-03-03 10:43:10 -0600

    changed box sizes

--- scripts/functions.js
+++ scripts/functions.js
@@ -9,7 +9,7 @@ export const arrayObjectDisplay = (array) => {
         if (item.population){
             const citiesHTML = document.querySelector(".cities")
             citiesHTML.innerHTML += `
-                <li>${item.name}</li>
+                <li><b>${item.name}</b></li>
                 <li>${item.population}</li>
                 <li>${item.description}</li>
                 <li><a href='${item.link}' target='_blank'>More Information</a></li><br>
@@ -33,7 +33,7 @@ export const arrayObjectDisplay = (array) => {
         if (item.occupation){
             const citizensHTML = document.querySelector(".citizens")
             citizensHTML.innerHTML += `
-                <li>${item.name}</li>
+                <li><b>${item.name}</b></li>
                 <li>${item.occupation}</li>
                 <li>${item.description}</li>
                 <li><a href='${item.link}' target='_blank'>More Information</a></li><br>
